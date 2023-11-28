0cccc59e7777b463b96a113a8ec44e59e2786e93
Francis Murungi <francis_951@live.com>
2023-11-28 07:42:33 -0800

    add functions and conditional staement files

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    num = number % -10
+else:
+    num = number % 10
+if num > 5:
+    print('Last digit of {} and is {} greater than 5'.format(number, num))
+elif num == 0:
+    print('Last digit of {} and is {} zero'.fromat(number, num))
+elif num < 6 and not 0:
+    print('Last digit of {} is {} less than 6 and not 0'.format(number, num))
+else:
+    print("Last digit of {} is {} less than 6 and not 0".format(number, num))
